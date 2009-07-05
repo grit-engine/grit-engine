ef6a5ad08b5d4e6a333ac9e46f47878fd30f55ba
Dave Cunningham <sparkprime@gmail.com>
2009-07-05 05:26:44 +0000

    Move dependency generation into makefile as 'make depend'

--- grit_core/linux/makedepend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-makedepend -Y -f ../src/common.mk ../src/{,linux,win32}/*.c++
-
-sed -i 's_^[.][.]/src/\(linux\|win32\)\?/__g' ../src/common.mk
-
--- gtasa/linux/makedepend.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-makedepend -Y -f ../src/common.mk -I ../../grit_core/src ../src/*.c++
-
-sed -i 's_^[.][.]/src/__g' ../src/common.mk
