7656ce82af9d579b7d83b2ad000ee033a2bfa96d
Dave Cunningham <sparkprime@gmail.com>
2014-09-26 03:56:14 +0000

    Add dropdown menus to grit book, and links to switch between web / complete mode

--- trunk/grit_core/doc/grit_book/unparse_html.py
+++ trunk/grit_core/doc/grit_book/unparse_html.py
@@ -7,49 +7,6 @@ import lxml.etree as ET
 
 from translate_xml import *
 
-def GeneratePage(title, content, book):
-    s = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-    <title>Grit Manual - ''' + title + '''</title>
-    <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
-    <link rel="stylesheet" type="text/css" href="grit_book.css" />
-    <link rel="icon" type="image/png" href="logo_tiny.png" />
-</head>
-
-<body>
-
-<div class="page">
-
-
-<div class="header">
-    <div class="logo"> </div>
-    <div class="navbar">'''
-    if book:
-        s += '\n    <div class="navitem"><a class="navitem" href="index.html">Contents</a></div>'
-        for chapter_index, chapter in enumerate(book):
-            filename = chapter.id + '.html'
-            s += '\n        <div class="navitem"><a class="navitem" href="%s">%d. %s</a></div>' % (filename, chapter_index + 1, chapter.title)
-    else:
-        s += '\n    <div class="navitem"><a class="navitem" href="complete.html">Complete Manual</a></div>'
-    s += '''
-    </div>
-</div>
-
-
-<div class="content">
-
-''' + content + '''</div> <!-- content -->
-
-
-</div> <!-- page -->
-
-</body>
-
-</html>
-'''
-    return s
 
 def GetParentIndex(node):
     i = 0
@@ -69,8 +26,6 @@ def GetPath(node):
         here = [str(GetParentIndex(node))]
     return GetPath(node.parent) + here
 
-def GetPathString(node):
-    return '.'.join(GetPath(node))
 
 def GetLinkToNode(target):
     the_file = GetOutputFile(target)
@@ -78,6 +33,120 @@ def GetLinkToNode(target):
         return '%s.html' % the_file.id
     else:
         return '%s.html#%s' % (the_file.id, target.id)
+
+
+def GetPathString(node):
+    return '.'.join(GetPath(node))
+
+
+def GeneratePage(title, content, book):
+    s = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <title>Grit Manual - ''' + title + '''</title>
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="grit_book.css" />
+    <link rel="icon" type="image/png" href="logo_tiny.png" />
+    <script type="text/javascript">
+
+        var menu_timeout = null;
+
+        function set_visible(menu, b)
+        {
+            var category = menu.children[0];
+            var dropdown = menu.children[1];
+            if (dropdown != null) {
+                dropdown.style.visibility = b ? 'visible' : 'hidden';
+            }
+        }
+
+        function menu_open(el)
+        {
+            menu_close_all();
+            var menu = el;
+            while (menu.id === "") {
+                menu = menu.parentNode;
+            }
+            set_visible(menu, true);
+        }
+
+        function menu_leave()
+        {
+            if (menu_timeout != null) {
+                window.clearTimeout(menu_timeout);
+            }
+            menu_timeout = window.setTimeout(menu_close_all, 300);
+        }
+
+        function menu_close_all()
+        {
+            var mb = document.getElementById("menubar");
+            if (mb == null) return;
+            var menus = mb.children;
+            for (var i = 0 ; i < menus.length ; ++i) {
+                set_visible(menus[i], false);
+            }
+            if (menu_timeout != null) {
+                window.clearTimeout(menu_timeout);
+                window_timeout = null;
+            }
+        }
+
+        document.onclick = menu_close_all(); 
+
+    </script>
+</head>
+
+<body>
+
+<div class="page">
+
+
+<div class="header">'''
+    if book:
+        s += '\n    <div id="complete"><a href="complete.html">Complete Manual</a></div>'
+    else:
+        s += '\n    <div id="complete"><a href="index.html">Web Manual</a></div>'
+    s += '\n    <div class="logo"> </div>'
+    if book:
+        s += '\n<ol id="menubar">'
+        s += '''
+        <li id="menu_contents">
+            <a href="index.html" onmouseover="menu_open(this)" onmouseout="menu_leave()">Contents</a>
+        </li>
+'''
+        for chapter_index, chapter in enumerate(book):
+            s += '\n        <li id="%s">' % chapter.id
+            s += ('\n            <a href="%s.html" onmouseover="menu_open(this)" onmouseout="menu_leave()">%d. %s</a>'
+                  % (chapter.id, chapter_index + 1, escape(chapter.title)))
+            s += '\n            <div onmouseover="menu_open(this)" onmouseout="menu_leave()">'
+            for section in chapter:
+                if section.kind == 'Section':
+                    path_string = GetPathString(section)
+                    title = escape(section.title)
+                    url = GetLinkToNode(section)
+                    s += '\n                <a href="%s">%s. %s</a>' % (url, path_string, title)
+            s += '\n            </div>'
+            s += '\n        </li>'
+        s += '\n        <div style="clear: both"></div>'
+        s += '\n    </ol>'
+
+    s += '''
+</div>
+
+<div class="content">
+
+''' + content + '''</div> <!-- content -->
+
+
+</div> <!-- page -->
+
+</body>
+
+</html>
+'''
+    return s
 
 def UnparseHtmlInlines(parent, inner=False):
     s = ""
