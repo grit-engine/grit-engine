66502fd784b43472202da2bbf6883d5587fda919
Dave Cunningham <sparkprime@gmail.com>
2016-05-06 23:37:08 -0400

    Script for easy copying of executables to media/

---
+++ copy_executables_to_media.bat
@@ -0,0 +1,5 @@
+
+copy Normal\launcher\launcher.exe media\Grit.exe
+copy Normal\engine\engine.exe media\Grit.dat
+copy Normal\extract\extract.exe media\Tools
+copy Normal\GritXMLConverter\GritXMLConverter.exe media\Tools
