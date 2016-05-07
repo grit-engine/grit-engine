0c474441229860667d3838e94d0a425319ba99d4
Dave Cunningham <dcunnin@google.com>
2016-05-06 23:12:31 -0400

    Add script for easy copying of executables to media

---
+++ copy_executables_to_media.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -v grit media/Grit.linux.x86_64
+cp -v gsl media/Tools/gsl.linux.x86_64
+cp -v extract media/Tools/extract.linux.x86_64
+cp -v grit_col_conv media/Tools/extract.linux.x86_64
+cp -v GritXMLConverter media/Tools/GritXMLConverter.x86_64
