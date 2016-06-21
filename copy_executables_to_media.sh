1946c449508fe368ec9b6b7f7f21d1b22b814691
Dave Cunningham <dcunnin@google.com>
2016-06-21 18:28:37 -0400

    Add a sanity check if there is no media directory

--- copy_executables_to_media.sh
+++ copy_executables_to_media.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! -d media ] ; then
+    echo 'You first need to clone the game directory (media) from SVN:' >&2
+    echo 'svn clone https://svn.code.sf.net/p/gritengine/code media' >&2
+    exit 1
+fi
+
 cp -v grit media/Grit.linux.x86_64
 cp -v gsl media/Tools/gsl.linux.x86_64
 cp -v extract media/Tools/extract.linux.x86_64
