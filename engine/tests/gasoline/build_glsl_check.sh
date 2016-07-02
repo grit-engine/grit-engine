16f852d40fc975e358a55d46b690014a3a7a35eb
Dave Cunningham <sparkprime@gmail.com>
2016-07-01 22:35:29 -0400

    Use system glfw3 via pkg-config

--- engine/tests/gasoline/build_glsl_check.sh
+++ engine/tests/gasoline/build_glsl_check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -Wall -Wextra -std=c++11 -I /usr/local/include -L /usr/local/lib glsl_check.cpp -o glsl_check -lGL -lglfw3 -lX11 -lXcursor -lXrandr -lXi -lXxf86vm -lpthread -ldl -lXinerama
+g++ -Wall -Wextra -std=c++11 glsl_check.cpp -o glsl_check -lGL `pkg-config --cflags --libs glfw3` -lX11 -lXcursor -lXrandr -lXi -lXxf86vm -lpthread -ldl -lXinerama
