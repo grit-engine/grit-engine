f720d6e05e1fc08049f2b31f32a4105192d0a46d
Dave Cunningham <dcunnin@google.com>
2016-08-16 21:31:06 -0400

    grit_book Lua & Gasoline syntax highlighting

---
+++ engine/doc/grit_book/pygments_lexers.py
@@ -0,0 +1,184 @@
+import re
+
+from pygments.lexer import Lexer, RegexLexer, ExtendedRegexLexer, \
+     LexerContext, include, combined, do_insertions, bygroups, using
+from pygments.token import Error, Text, Other, \
+     Comment, Operator, Keyword, Name, String, Number, Generic, Punctuation
+from pygments.util import get_bool_opt, get_list_opt, shebang_matches
+from pygments import unistring as uni
+
+
+class GritLuaLexer(RegexLexer):
+    """
+    For Grit Lua source code.
+
+    Additional options accepted:
+
+    `func_name_highlighting`
+        If given and ``True``, highlight builtin function names
+        (default: ``True``).
+    `disabled_modules`
+        If given, must be a list of module names whose function names
+        should not be highlighted. By default all modules are highlighted.
+
+        To get a list of allowed modules have a look into the
+        `_luabuiltins` module:
+
+        .. sourcecode:: pycon
+
+            >>> from pygments.lexers._luabuiltins import MODULES
+            >>> MODULES.keys()
+            ['string', 'coroutine', 'modules', 'io', 'basic', ...]
+    """
+
+    name = 'GritLua'
+    aliases = []
+    filenames = ['*.lua', '*.wlua']
+    mimetypes = ['text/x-grit-lua', 'application/x-grit-lua']
+
+    tokens = {
+        'root': [
+            # lua allows a file to start with a shebang
+            (r'#!(.*?)$', Comment.Preproc),
+            (r'', Text, 'base'),
+        ],
+        'base': [
+            (r'(?s)--\[(=*)\[.*?\]\1\]', Comment.Multiline),
+            ('--.*$', Comment.Single),
+
+            (r'(?i)(\d*\.\d+|\d+\.\d*)(e[+-]?\d+)?', Number.Float),
+            (r'(?i)\d+e[+-]?\d+', Number.Float),
+            ('(?i)0x[0-9a-f]*', Number.Hex),
+            (r'\d+', Number.Integer),
+
+            (r'\n', Text),
+            (r'[^\S\n]', Text),
+            # multiline strings
+            (r'(?s)\[(=*)\[.*?\]\1\]', String),
+
+            (r'(==|~=|<=|>=|\.\.\.|\.\.|[=+\-*/%^<>#])', Operator),
+            (r'[\[\]\{\}\(\)\.,:;]', Punctuation),
+            (r'(and|or|not)\b', Operator.Word),
+
+            ('(break|do|else|elseif|end|for|if|in|repeat|return|then|until|'
+             r'while)\b', Keyword),
+            (r'(local)\b', Keyword.Declaration),
+            (r'(true|false|nil)\b', Keyword.Constant),
+            (r'(vec((tor)?[2-3]?))\b', Keyword.Type),
+
+            (r'(function)\b', Keyword, 'funcname'),
+
+            (r'[A-Za-z_][A-Za-z0-9_]*(\.[A-Za-z_][A-Za-z0-9_]*)?', Name),
+
+            ("'", String.Single, combined('stringescape', 'sqs')),
+            ('"', String.Double, combined('stringescape', 'dqs')),
+            ('`', String.Double, combined('stringescape', 'path')),
+        ],
+
+        'funcname': [
+            (r'\s+', Text),
+            ('(?:([A-Za-z_][A-Za-z0-9_]*)(\.))?([A-Za-z_][A-Za-z0-9_]*)',
+             bygroups(Name.Class, Punctuation, Name.Function), '#pop'),
+            # inline function
+            ('\(', Punctuation, '#pop'),
+        ],
+
+        # if I understand correctly, every character is valid in a lua string,
+        # so this state is only for later corrections
+        'string': [
+            ('.', String)
+        ],
+
+
+        'stringescape': [
+            (r'''\\([abfnrtv\\"']|\d{1,3})''', String.Escape)
+        ],
+
+        'sqs': [
+            ("'", String, '#pop'),
+            include('string')
+        ],
+
+        'dqs': [
+            ('"', String, '#pop'),
+            include('string')
+        ],
+
+        'path': [
+            ('`', String, '#pop'),
+            include('string')
+        ],
+    }
+
+    def __init__(self, **options):
+        self.func_name_highlighting = get_bool_opt(
+            options, 'func_name_highlighting', True)
+        self.disabled_modules = get_list_opt(options, 'disabled_modules', [])
+
+        self._functions = set()
+        if self.func_name_highlighting:
+            from pygments.lexers._luabuiltins import MODULES
+            for mod, func in MODULES.iteritems():
+                if mod not in self.disabled_modules:
+                    self._functions.update(func)
+        RegexLexer.__init__(self, **options)
+
+    def get_tokens_unprocessed(self, text):
+        for index, token, value in \
+            RegexLexer.get_tokens_unprocessed(self, text):
+            if token is Name:
+                if value in self._functions:
+                    yield index, Name.Builtin, value
+                    continue
+                elif '.' in value:
+                    a, b = value.split('.')
+                    yield index, Name, a
+                    yield index + len(a), Punctuation, u'.'
+                    yield index + len(a) + 1, Name, b
+                    continue
+            yield index, token, value
+
+
+
+
+class GasolineLexer(RegexLexer):
+    """
+    For Gasoline shader code.
+
+    """
+
+    name = 'Gasoline'
+    aliases = ['gsl']
+    filenames = ['*.gsl']
+    mimetypes = ['text/x-gasoline', 'application/x-gasoline']
+
+    tokens = {
+        'root': [
+            (r'', Text, 'base'),
+        ],
+        'base': [
+            (r'//(\n|(.|\n)*?[^\\]\n)', Comment.Single),
+            (r'/(\\\n)?[*](.|\n)*?[*](\\\n)?/', Comment.Multiline),
+
+            (r'(?i)(\d*\.\d+|\d+\.\d*)(e[+-]?\d+)?', Number.Float),
+            (r'(?i)\d+e[+-]?\d+', Number.Float),
+            ('(?i)0x[0-9a-f]*', Number.Hex),
+            (r'\d+', Number.Integer),
+
+            (r'[^\S]', Text),
+
+            (r'(&&|\|\||==|!=|<=|>=|[!*/%+<>=-])', Operator),
+            (r'[\[\]\{\}\(\)\.,:;]', Punctuation),
+            (r'(and|or|not)\b', Operator.Word),
+
+            (r'(discard|else|if|for|discard)\b', Keyword),
+            (r'((Float|Int)[2-4n]?|FloatTexture([1-3]D|Cube)|Bool)\b', Keyword.Type),
+            (r'(var)\b', Keyword.Declaration),
+            (r'(true|false)\b', Keyword.Constant),
+
+            (r'(mat|body|global|vert|out|frag)\b', Name.Builtin.Pseudo),
+
+            (r'[A-Za-z_][A-Za-z0-9_]*', Name),
+        ],
+    }
+
