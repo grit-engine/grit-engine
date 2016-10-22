b2b89769d2d63c817b700e4641b14bd2ec56ebe8
Dave Cunningham <sparkprime@gmail.com>
2016-10-22 14:09:00 -0400

    Make copyright notices less vain

--- engine/gfx/gfx_gasoline_backend.cpp
+++ engine/gfx/gfx_gasoline_backend.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) David Cunningham and the Grit Game Engine project 2015;
+/* Copyright (c) The Grit Game Engine authors 2016;
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
