232b9fcd83bfda444feea6cbe229694522cb1b8b
Dave Cunningham <sparkprime@gmail.com>
2016-11-06 12:02:06 -0500

    Fix build bat, add script for copying from bucket to media

---
+++ copy_executables_from_bucket.sh
@@ -0,0 +1,5 @@
+gsutil cp gs://grit-engine/engine.exe media/Grit.dat
+gsutil cp gs://grit-engine/launcher.exe media/Grit.exe
+gsutil cp gs://grit-engine/extract.exe media/Tools
+gsutil cp gs://grit-engine/GritXMLConverter.exe media/Tools
+
