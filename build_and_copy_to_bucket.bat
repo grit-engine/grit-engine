26bced301d30bf2fc346f2848862bda14a074809
Dave Cunningham <sparkprime@gmail.com>
2016-11-06 11:42:02 -0500

    Add bat for building solution from CLI

---
+++ build_and_copy_to_bucket.bat
@@ -0,0 +1,10 @@
+SET PATH=C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin;%PATH%;
+CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
+git pull --recurse-submodules
+git submodule update
+msbuild /p:Configuration=Normal
+
+gsutil cp Normal/engine/engine.exe gs://grit-engine/
+gsutil cp Normal/launcher/launcher.exe gs://grit-engine/
+gsutil cp Normal/extract/extract.exe gs://grit-engine/
+gsutil cp Normal/GritXMLConverter/GritXMLConverter.exe gs://grit-engine/
