492c8cdde35f69330c241cfedaccf6907ba12723
Dave Cunningham <sparkprime@gmail.com>
2016-11-06 21:32:10 -0500

    Fix typo

--- copy_executables_from_bucket.sh
+++ copy_executables_from_bucket.sh
@@ -1,5 +1,5 @@
 gsutil cp gs://grit-engine/Normal/engine/engine.exe media/Grit.dat
 gsutil cp gs://grit-engine/Normal/launcher/launcher.exe media/Grit.exe
 gsutil cp gs://grit-engine/Normal/extract/extract.exe media/Tools
-gsutil cp gs://grit-engine/Normal/GritXMLConverted/GritXMLConverter.exe media/Tools
+gsutil cp gs://grit-engine/Normal/GritXMLConverter/GritXMLConverter.exe media/Tools
 
